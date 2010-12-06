ca5a5b1ae0ecc232f18757998bf9bc95b3221fe3
A. Bram Neijt <bneijt@gmail.com>
2010-12-06 20:43:35 +0100

    Introducing setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='Datajunction',
+      version='0.1',
+      description='Backup utility using hash based chunks and json metadata',
+      author='A. Bram Neijt',
+      author_email='bram@neijt.nl',
+      url='http://www.logfish.net/pr/datajunction',
+      #packages=['', 'distutils.command'],
+      scripts=['scripts/datajunction_write' ],
+
+     )
+
