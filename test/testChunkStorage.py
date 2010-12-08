9093316a2f2d8425d5206a2c3503b49be346ece5
A. Bram Neijt <bneijt@gmail.com>
2010-12-08 22:09:55 +0100

    Bugfix in delta exception

--- test/testChunkStorage.py
+++ test/testChunkStorage.py
@@ -2,7 +2,7 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../src')) #UGLY!
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src/datajunction')) #UGLY!
 import chunkstore
 
 import unittest
